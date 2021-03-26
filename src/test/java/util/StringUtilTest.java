ce31481b551beda3f90b51140c60eacc8b89343b
Elena Piaggio <elenapiaggio@gmail.com>
2021-03-26 17:11:16 +0100

    ADD first UnitTest

--- src/test/java/util/StringUtilTest.java
+++ src/test/java/util/StringUtilTest.java
@@ -1,4 +1,14 @@
-import static org.junit.Assert.*;
+package util;
+
+import org.junit.Assert;
+import org.junit.Test;
+
 public class StringUtilTest {
-  
+
+  @Test
+  public void testRepeat() {
+    Assert.assertEquals("holaholahola", StringUtil.repeat("hola", 3));
+    Assert.assertEquals("hola", StringUtil.repeat("hola", 1));
+  }
+
 }
