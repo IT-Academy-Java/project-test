126979e03bcaca73b2670684442fefdd61895b42
Elena Piaggio <elenapiaggio@gmail.com>
2021-03-27 18:02:40 +0100

    ADD junit test

--- src/main/java/util/StringUtil.java
+++ src/main/java/util/StringUtil.java
@@ -3,6 +3,11 @@ package util;
 public class StringUtil {
 
   public static String repeat(String str, int times){
+
+    if(times < 0 ){
+      throw new IllegalArgumentException("Negative times not allowed");
+    }
+
     String result = "";
     for (int i = 0; i < times; i++) {
       result+= str;
