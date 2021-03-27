b89d5981d91b821b84f4630aea1fd09f189a6a76
Elena Piaggio <elenapiaggio@gmail.com>
2021-03-27 19:29:13 +0100

    ADD PaymentProcessorTest

--- src/test/java/payments/PaymentProcessorTest.java
+++ src/test/java/payments/PaymentProcessorTest.java
@@ -1,4 +1,21 @@
+package payments;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
 import static org.junit.Assert.*;
+
 public class PaymentProcessorTest {
-  
+
+  @Test
+  public void paymentIsCorrect(){
+    PaymentGateway paymentGateway = Mockito.mock(PaymentGateway.class);
+    Mockito.when(paymentGateway.requestPayment(Mockito.any()))
+            .thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.OK));
+
+    PaymentProcessor paymentProcessor = new PaymentProcessor(paymentGateway);
+
+    assertTrue(paymentProcessor.makePayment(1000));
+  }
+
 }
