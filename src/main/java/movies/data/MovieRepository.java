7f954b33f8329f829abf0d55908272113ea7f8b3
Elena Piaggio <elenapiaggio@gmail.com>
2021-03-27 21:56:17 +0100

    ADD repository

--- src/main/java/movies/data/MovieRepository.java
+++ src/main/java/movies/data/MovieRepository.java
@@ -1,2 +1,12 @@
-package movies.data;public class MovieRepository {
+package movies.data;
+
+import movies.model.Movie;
+
+import java.util.Collection;
+
+public interface MovieRepository {
+
+  Movie findById(long id);
+  Collection<Movie> findAll();
+  void saveOrUpdate(Movie movie);
 }
