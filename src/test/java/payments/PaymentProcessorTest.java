5f7e344fe352cf41b27fa7aaa40869839669d818
Elena Piaggio <elenapiaggio@gmail.com>
2021-03-27 20:02:50 +0100

    refactor PaymentProcessorTest

--- src/test/java/payments/PaymentProcessorTest.java
+++ src/test/java/payments/PaymentProcessorTest.java
@@ -1,5 +1,6 @@
 package payments;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
@@ -7,24 +8,27 @@ import static org.junit.Assert.*;
 
 public class PaymentProcessorTest {
 
+  private PaymentGateway paymentGateway;
+  private PaymentProcessor paymentProcessor;
+
+  @Before
+  public void setup(){
+    paymentGateway = Mockito.mock(PaymentGateway.class);
+    paymentProcessor = new PaymentProcessor(paymentGateway);
+  }
+
   @Test
   public void paymentIsCorrect(){
-    PaymentGateway paymentGateway = Mockito.mock(PaymentGateway.class);
     Mockito.when(paymentGateway.requestPayment(Mockito.any()))
             .thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.OK));
-
-    PaymentProcessor paymentProcessor = new PaymentProcessor(paymentGateway);
 
     assertTrue(paymentProcessor.makePayment(1000));
   }
 
   @Test
   public void paymentIsWrong(){
-    PaymentGateway paymentGateway = Mockito.mock(PaymentGateway.class);
     Mockito.when(paymentGateway.requestPayment(Mockito.any()))
             .thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.ERROR));
-
-    PaymentProcessor paymentProcessor = new PaymentProcessor(paymentGateway);
 
     assertFalse(paymentProcessor.makePayment(1000));
   }
